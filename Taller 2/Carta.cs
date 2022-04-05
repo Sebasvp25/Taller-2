37bdbf3cd4b49233685f8f62ca521b5367d448b2
Sofía Díaz Z <67989042+SofiaDiazZ@users.noreply.github.com>
2022-04-05 10:45:09 -0500

    Update Carta.cs

--- Taller 2/Carta.cs
+++ Taller 2/Carta.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Carta
+namespace ElJugonSAS
 {
-	public Carta()
+	public class Carta
 	{
+		private string nombre;
+		private string rareza;
+		private int costo;
+
+		public string Nombre { get => nombre; }
+		public string Rareza { get => rareza; set => rareza = value; }
+		public int Costo { get => costo; }
+
+		public Carta(string nombre, int costo, string rareza)
+		{
+			this.nombre = nombre;
+			this.costo = costo;
+			this.rareza = rareza;
+		}
+
 	}
 }
